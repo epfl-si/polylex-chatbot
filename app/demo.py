d533d26aac08b693afe251681f8acd92b6fa0a3d
Saskya Panchaud <saskya.panchaud@epfl.ch>
2026-05-01 14:51:24 +0200

    [feature] Update theme of UI and update generation part of RAG with actual state of DB and improve prompt template

--- app/demo.py
+++ app/demo.py
@@ -7,61 +7,114 @@ from langchain_classic.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langfuse import Langfuse
 from langfuse.langchain import CallbackHandler
+from qdrant_client.http.models import SearchParams
+from langdetect import detect
 
 langfuse = Langfuse()
 
 @cl.on_message
 async def main(message: cl.Message):
-    """
-    This function is called every time a user inputs a message in the UI.
-    Args:
-        message: The user's message.
-    Returns:
-        None.
-    """
-
     langfuse_handler = CallbackHandler()
+
+    # TODO : secure content send from the user (for language detection + send to LLM)
+    lang = detect(message.content)
+    if lang not in ["fr", "en"]:
+        # TODO : handle this error with call to LLM + same for question out of context
+        print(f"Language {lang} is not supported.")  # TODO
+        return
 
     embeddings = OpenAIEmbeddings(
         model=os.getenv("MODEL_EMBEDDINGS_NAME"),
         base_url=os.getenv("MODELS_BASE_URL"),
         api_key=os.getenv("MODEL_EMBEDDINGS_API_KEY")
     )
-    sparse = FastEmbedSparse(model_name=os.getenv("MODEL_SPARSE_NAME"))
-    qdrant = QdrantVectorStore.from_existing_collection(
-        url=os.getenv("QDRANT_URL"),
-        embedding=embeddings,
-        sparse_embedding=sparse,
-        collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
-        retrieval_mode=RetrievalMode.HYBRID
-    )
+
+    # TODO : improve prompts (simple to complex)
+    prompt_template_en = """
+You are an AI assistant specialized in answering questions using provided context.
+
+Rules:
+- You MUST base your answer ONLY on the provided context.
+- If the context does not contain the answer, say "I don't know".
+- Do NOT use prior knowledge unless explicitly allowed.
+- Cite relevant parts of the context when possible.
+- Be concise and factual.
+
+Context:
+{context}
+
+Question:
+{input}
+"""
+
+    prompt_template_fr = """
+Vous êtes un assistant IA spécialisé dans la réponse aux questions à partir d’un contexte fourni.
+
+Règles :
+- Vous DEVEZ baser votre réponse UNIQUEMENT sur le contexte fourni.
+- Si le contexte ne contient pas la réponse, dites "Je ne sais pas".
+- N’utilisez PAS de connaissances externes sauf si cela est explicitement autorisé.
+- Citez les parties pertinentes du contexte lorsque c’est possible.
+- Soyez concis et factuel.
+
+Contexte :
+{context}
+
+Question :
+{input}
+    """
+
+    config_by_lang = {
+        "fr": {
+            "model": FastEmbedSparse(
+                model_name=os.getenv("MODEL_SPARSE_NAME"),
+                avg_len=os.getenv("MODEL_SPARSE_AVG_LEN_FR"),
+                language="french"
+            ),
+            "vector_name" : os.getenv("MODEL_SPARSE_VECTOR_NAME_FR"),
+            "prompt": prompt_template_fr
+        },
+        "en": {
+            "model": FastEmbedSparse(
+                model_name=os.getenv("MODEL_SPARSE_NAME"),
+                avg_len=os.getenv("MODEL_SPARSE_AVG_LEN_EN"),
+                language="english"
+            ),
+            "vector_name" : os.getenv("MODEL_SPARSE_VECTOR_NAME_EN"),
+            "prompt": prompt_template_en
+        }
+    }
+
+    retrievers = {
+        lang: QdrantVectorStore.from_existing_collection(
+            url=os.getenv("QDRANT_URL"),
+            embedding=embeddings,
+            sparse_embedding=cfg["model"],
+            collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
+            retrieval_mode=RetrievalMode.HYBRID,
+            vector_name="dense",
+            sparse_vector_name=cfg["vector_name"],
+        ).as_retriever(
+            search_type="similarity",
+            search_kwargs={
+                "k": int(os.getenv("QDRANT_NB_CHUNKS_RETRIEVED")),
+                "search_params": SearchParams(exact=True)
+            }
+        )
+        for lang, cfg in config_by_lang.items()
+    }
 
     llm = OpenAI(
         model=os.getenv("MODEL_LLM_NAME"),
         base_url=os.getenv("MODELS_BASE_URL"),
         api_key=os.getenv("MODEL_LLM_API_KEY"),
-        max_tokens=int(os.getenv("MAX_TOKENS_LLM"))
+        max_tokens=int(os.getenv("MAX_TOKENS_LLM")),
+        temperature=float(os.getenv("TEMPERATURE_LLM"))
     )
 
-
-    retriever = qdrant.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": int(os.getenv("QDRANT_NB_CHUNKS_RETRIEVED"))}
-    )
-
-    prompt_template = """
-        Context:
-        {context}
-
-        Question:
-        {input}
-
-        Answer in French using only the context above.
-        If the answer is not in the context, say: "I can't help you...".
-        """
-    prompt = ChatPromptTemplate.from_template(prompt_template)
+    prompt = ChatPromptTemplate.from_template(config_by_lang[lang]["prompt"])
     combine_docs_chain = create_stuff_documents_chain(llm, prompt)
-    retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+    retrieval_chain = create_retrieval_chain(retrievers[lang], combine_docs_chain)
     response = retrieval_chain.invoke(
         {"input": message.content},
         config={"callbacks": [langfuse_handler]}
@@ -76,6 +129,8 @@ async def main(message: cl.Message):
             cl.Action(name="dislike", payload={"trace_id": trace_id}, label="👎", tooltip="The answer was useless!"),
         ]
     ).send()
+
+    # TODO : add sources in specific box (out of response of LLM)
 
 @cl.action_callback("like")
 async def like(action):
